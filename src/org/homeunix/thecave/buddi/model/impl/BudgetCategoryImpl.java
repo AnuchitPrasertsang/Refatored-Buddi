b9930c46de9aa4478aaaab4f9e81fc7d1a1cbe44
Jackson Zhang <zbcjackson@gmail.com>
2014-04-11 14:46:58 +0800

    Extract getAmount with period

--- src/org/homeunix/thecave/buddi/model/impl/BudgetCategoryImpl.java
+++ src/org/homeunix/thecave/buddi/model/impl/BudgetCategoryImpl.java
@@ -105,36 +105,38 @@ public class BudgetCategoryImpl extends SourceImpl implements BudgetCategory {
 		if (startDate.after(endDate))
 			throw new RuntimeException("Start date cannot be before End Date!");
 
-        Period period = new Period(startDate, endDate);
+        return getAmount(new Period(startDate, endDate));
+    }
 
-		//If Start and End are in the same budget period
-		if (getBudgetPeriodType().getStartOfBudgetPeriod(period.getStartDate()).equals(
-				getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()))){
-            return (long) getAmountInPeriod(period.getStartDate(), period.getEndDate());
-		}
-		 
-		//If the area between Start and End overlap at least two budget periods.
-		if (getBudgetPeriodType().getStartOfNextBudgetPeriod(period.getStartDate()).equals(
-				getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()))
-				|| getBudgetPeriodType().getStartOfNextBudgetPeriod(period.getStartDate()).before(
-						getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()))){
+    private long getAmount(Period period) {
+        //If Start and End are in the same budget period
+        if (getBudgetPeriodType().getStartOfBudgetPeriod(period.getStartDate()).equals(
+                getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()))){
+return (long) getAmountInPeriod(period.getStartDate(), period.getEndDate());
+        }
 
-            double totalStartPeriod = getAmountInPeriod(period.getStartDate(), getBudgetPeriodType().getEndOfBudgetPeriod(period.getStartDate()));
+        //If the area between Start and End overlap at least two budget periods.
+        if (getBudgetPeriodType().getStartOfNextBudgetPeriod(period.getStartDate()).equals(
+                getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()))
+                || getBudgetPeriodType().getStartOfNextBudgetPeriod(period.getStartDate()).before(
+                getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()))){
 
-			double totalInMiddle = 0;
-			for (String periodKey : getBudgetPeriods(
-					getBudgetPeriodType().getStartOfNextBudgetPeriod(period.getStartDate()),
-					getBudgetPeriodType().getStartOfPreviousBudgetPeriod(period.getEndDate()))) {
-				totalInMiddle += getAmount(getPeriodDate(periodKey));
-			}
+double totalStartPeriod = getAmountInPeriod(period.getStartDate(), getBudgetPeriodType().getEndOfBudgetPeriod(period.getStartDate()));
 
-            double totalEndPeriod = getAmountInPeriod(getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()), period.getEndDate());
+            double totalInMiddle = 0;
+            for (String periodKey : getBudgetPeriods(
+                    getBudgetPeriodType().getStartOfNextBudgetPeriod(period.getStartDate()),
+                    getBudgetPeriodType().getStartOfPreviousBudgetPeriod(period.getEndDate()))) {
+                totalInMiddle += getAmount(getPeriodDate(periodKey));
+            }
 
-			return (long) (totalStartPeriod + totalInMiddle + totalEndPeriod);
-		}
+double totalEndPeriod = getAmountInPeriod(getBudgetPeriodType().getStartOfBudgetPeriod(period.getEndDate()), period.getEndDate());
 
-		throw new RuntimeException("You should not be here.  We have returned all legitimate numbers from getAmount(Date, Date) in BudgetCategoryImpl.  Please contact Wyatt Olson with details on how you got here (what steps did you perform in Buddi to get this error message).");
-	}
+            return (long) (totalStartPeriod + totalInMiddle + totalEndPeriod);
+        }
+
+        throw new RuntimeException("You should not be here.  We have returned all legitimate numbers from getAmount(Date, Date) in BudgetCategoryImpl.  Please contact Wyatt Olson with details on how you got here (what steps did you perform in Buddi to get this error message).");
+    }
 
     private double getAmountInPeriod(Date startDate, Date endDate) {
         long amount = getAmount(startDate);
